2e50babc80d30f38a4da3a43bc40c8adf3aa86e2
Ckott <diegoangeles130703@gmail.com>
2026-04-03 01:56:04 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors({
+    credentials: true,
+    origin: "*"
+}))
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Chatbot ALI - is running')
+})
+
+
+export default app
