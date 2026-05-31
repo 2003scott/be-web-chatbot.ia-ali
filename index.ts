b76344352c8712f62779b7da4412f898357b1fac
Ckott <diegoangeles130703@gmail.com>
2026-05-31 04:42:16 -0500

    refactor server startup logic to conditionally listen on port based on Vercel environment

--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@
 import { env } from "./src/config";
 import app from "./src/app";
 
-app.listen(env.PORT, () => {
-    console.log(`Server is running on port ${env.PORT}`)
-})
+const isVercel = !!process.env.VERCEL;
+
+if (!isVercel) {
+    app.listen(env.PORT, () => {
+        console.log(`Server is running on port ${env.PORT}`);
+    });
+}
+
+export default app;
